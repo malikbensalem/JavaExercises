2b1eac97e633b8165731d9bfdcf2ac2477100efd
malikbensalem <60427086+malikbensalem@users.noreply.github.com>
2020-02-19 10:07:13 +0000

    set 2

--- ChickRab/src/com/qa/chickrab/Runner.java
+++ ChickRab/src/com/qa/chickrab/Runner.java
@@ -5,7 +5,7 @@ public class Runner {
         chickRab(99,24);
     }public static void chickRab(int h,int l){
         int c=2*h-l/2;
-        if (c>0h-c>0) {
+        if (c>0||h-c>0) {
             System.out.println("Chickens:"+c+"\nRabbits:"+(h-c));
             return;
         }
