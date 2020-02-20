bf88cedd5912d225479a0d47f0dce47c1f05403f
malik <malikbensalem@outlook.com>
2020-02-20 22:16:07 +0000

    set 4

---
+++ Primes/src/com/qa/primes/Runner.java
@@ -0,0 +1,31 @@
+package com.qa.primes;
+
+public class Runner {
+    public static void main(String args[]) {
+        System.out.println(primes(12));
+        System.out.println(primes(2));
+        System.out.println();
+
+    }
+
+    public static int primes(int max) {
+        int count = 0;
+        if (max > 1) {
+            boolean prime = true;
+            for (int i = 2; i <= max; i++, prime = true) {
+                for (int j = 2; j <= i / 2; j++) {
+                    if (i % j == 0) {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime) {
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
+}
+
