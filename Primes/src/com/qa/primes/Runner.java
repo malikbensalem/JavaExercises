0e711db7ceffe25aaf5ccc19c1db48c65e06f0b6
malikbensalem <60427086+malikbensalem@users.noreply.github.com>
2020-02-21 10:06:51 +0000

    set 4

--- Primes/src/com/qa/primes/Runner.java
+++ Primes/src/com/qa/primes/Runner.java
@@ -13,7 +13,7 @@ public class Runner {
         if (max > 1) {
             boolean prime = true;
             for (int i = 2; i <= max; i++, prime = true) {
-                for (int j = 2; j <= i / 2; j++) {
+                for (int j = 2; j <= (int)Math.sqrt(i); j++) {
                     if (i % j == 0) {
                         prime = false;
                         break;
